dbbe6ee49d326297705bb41be392fccba41055c6
Allart Matheo <147828172+Mallart@users.noreply.github.com>
2024-03-22 08:40:47 +0100

    Removed accents in comments

--- str.h
+++ str.h
@@ -13,28 +13,28 @@
 #define CAST_STR_TO_INT(str) str_to_int(str, str_len(str))
 #define CAST_STR_TO_FLOAT(str) str_to_float(str, str_len(str))
 
-// Renvoie le nombre d'éléments dans un tableau de pointeurs.
+// Renvoie le nombre d'elements dans un tableau de pointeurs.
 int ptr_array_len(void** _array);
-// Compte les occurrences de caractères dans une chaîne de caractères.
+// Compte les occurrences de caracteres dans une chaîne de caracteres.
 int str_count_char(char* str, char c);
-// Renvoie le nombre de caractères dans une chaîne de caractères terminée par un caractère nul.
+// Renvoie le nombre de caracteres dans une chaîne de caracteres terminee par un caractere nul.
 short str_len(char* string);
-// Renvoie une nouvelle chaîne identique à celle fournie.
+// Renvoie une nouvelle chaîne identique a celle fournie.
 char* str_copy(char* string, int length_to_copy);
-// Renvoie la position du caractère donné dans la chaîne, ou la longueur de la chaîne si ce caractère n'est pas présent.
+// Renvoie la position du caractere donne dans la chaîne, ou la longueur de la chaîne si ce caractere n'est pas present.
 int str_indexof(char* string, char character);
 // Renvoie 1 si les deux chaînes sont identiques, 0 sinon.
 bool str_compare(char* string_1, char* string_2, int length);
-// Renvoie un tableau de chaînes, séparées par un caractère supprimé à la fin du traitement.
+// Renvoie un tableau de chaînes, separees par un caractere supprime a la fin du traitement.
 char** str_split(char* string, char delimiter);
 // Convertit une chaîne en un entier.
 int str_to_int(char* string, int length);
-// Convertit une chaîne en un nombre à virgule flottante.
-// La partie flottante ignore tout texte présent dans la chaîne qui n'est pas constitué de chiffres.
+// Convertit une chaîne en un nombre a virgule flottante.
+// La partie flottante ignore tout texte present dans la chaîne qui n'est pas constitue de chiffres.
 float str_to_float(char* str, int length);
-// Supprime les tabulations, les retours à la ligne et les espaces avant le premier caractère visible et après le dernier.
+// Supprime les tabulations, les retours a la ligne et les espaces avant le premier caractere visible et apres le dernier.
 char* str_trim(char* string);
-// Crée une nouvelle chaîne basée sur celle donnée, mais commençant à un index donné et se terminant n caractères après.
+// Cree une nouvelle chaîne basee sur celle donnee, mais commençant a un index donne et se terminant n caracteres apres.
 char* str_substring(char* string, int start, int length);
 
 int power(int a, int b);
